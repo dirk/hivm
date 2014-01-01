1ac225a3af6ff510c3201148fcbe0d0cb70e59bb
Dirk Gadsden <dirk@esherido.com>
2013-12-31 23:21:40 -0500

    Start adding documentation

--- src/vm.h
+++ src/vm.h
@@ -1,19 +1,29 @@
 #ifndef HVM_VM_H
 #define HVM_VM_H
+/// @file vm.h
 
+/// VM instruction
 typedef char hvm_op;
 #define HVM_OP_SIZE sizeof(hvm_op)
 
 // Size of chunks to be allocated for storing bytecodes.
 #define HVM_GENERATOR_GROW_RATE 65536
 
+/**
+Stores instructions, constants, etc. for a chunk. Can then generate the
+appropriate bytecode for that chunk.
+*/
 typedef struct hvm_generator {
-  hvm_op* ops; // Memory for program
-  int p_bytes_used; // Number of bytes used in the program
-  int p_bytes_remaining; // Bytes remaining
-  int ip; // Instruction pointer to next index in ops to write an instruction
+  // nothing
 } hvm_generator_t;
 
+/**
+Generates bytecode.
+@memberof hvm_generator
+*/
+void hvm_generator_generate(hvm_generator_t*);
+
+/// Instance of the VM.
 typedef struct hvm_vm {
   // root stack
   // code
@@ -22,8 +32,9 @@ typedef struct hvm_vm {
   // object space
 } hvm_vm_t;
 
+/// Valid opcodes.
 typedef enum hvm_opcode {
-  HVM_OP_NOOP = 0,
+  HVM_OP_NOOP = 0, ///< Does nothing.
   HVM_OP_GOTO = 1
 } hvm_opcode_t;
 
