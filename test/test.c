51fddd8bc0fb2a2d7d3cd8a204143b814293cc2f
Dirk Gadsden <dirk@esherido.com>
2014-03-07 17:39:34 -0500

    Byte fiddling

--- test/test.c
+++ test/test.c
@@ -4,7 +4,22 @@
 
 // gcc test.c -g -o test
 
-int main(int argc, char *argv[]) {
+void byte_fiddling() {
+  uint32_t i = 0x0800;
+  printf("i: 0x%lx\n", (unsigned long)i);
+  
+  void *p = &i;
+  for(int o = 0; o < 4; o++) {
+    // These are equivalent.
+    //unsigned char c = ((unsigned char*)(&i))[o];
+    unsigned char c  = *((unsigned char*)&i + o);
+    // Have to cast to unsigned char before offsetting. This will make it go by
+    // bytes instead of ints (4 bytes).
+    printf("c[%d]: 0x%hhx\n", o, c);
+  }
+}
+
+void memcpy_fiddling() {
   // Testing a little memcpy
   unsigned char c[8];
   
@@ -31,6 +46,10 @@ int main(int argc, char *argv[]) {
   // Proof that no conversion occurred.
   uint64_t g = *((uint64_t*)&f);
   printf("g: %lld\n", g);
-  
+}
+
+int main(int argc, char *argv[]) {
+  // memcpy_fiddling();
+  byte_fiddling();
   return 0;
 }
