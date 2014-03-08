54ae37ec93f1e08130aeb1f94c522dabd43a4898
Dirk Gadsden <dirk@esherido.com>
2014-03-07 23:32:29 -0500

    Improve documentation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ For now you'll probably just want to read over the [notes](NOTES.md) and [instru
 
 Virtual machines have become a new layer of abstraction between the programmer and the machine their code runs on. The ecosystem of virtual machines is growing and the machines themselves are becoming increasingly more complex. Furthermore, virtual machines have almost always been closely bound to their "native tongue": the language they were originally designed to execute. Running "non-native" languages on these machines is cumbersome and often incurs a penalty in performance and/or functionality.
 
-The Hivm project aims to overcome these and provide a new, better virtual machines for a variety of static, dynamic, and hybrid languages:
+The Hivm project aims to overcome these and provide a new, better virtual machine for the execution of static, dynamic, and hybrid languages:
 
 1. **No native tongue**: Hivm instead provides a solid, dependable, performant base for implementing languages.
 2. **Minimize complexity**: bootstrapping a language, interacting with the machine, writing platform-native extensions, and the like should not be hard.
--- src/vm.h
+++ src/vm.h
@@ -14,38 +14,40 @@ typedef uint64_t hvm_instruction;
 /// Size of chunks to be allocated for storing bytecodes.
 #define HVM_GENERATOR_GROW_RATE 65536
 
+/// Initial size (in bytes) for program data.
+/// @relates hvm_vm
 #define HVM_PROGRAM_INITIAL_SIZE 16384
 
-/**
-@brief Chunk of instruction code and data (constants, etc.).
-*/
+/// @brief Chunk of instruction code and data (constants, etc.).
 typedef struct hvm_chunk {
   
 } hvm_chunk;
 
-/**
-@brief Stores instructions, constants, etc. for a chunk. Can then generate the
-       appropriate bytecode for that chunk.
-*/
+/// @brief Stores instructions, constants, etc. for a chunk. Can then generate the
+///        appropriate bytecode for that chunk.
 typedef struct hvm_generator {
   // nothing
 } hvm_generator;
 
-/**
-Generates bytecode.
-@memberof hvm_generator
-*/
+/// Generates bytecode.
+/// @memberof hvm_generator
 void hvm_generator_bytecode(hvm_generator*);
 
-/// Constant pool maps an integer to a constant
-// Can store approximately 4 billion constants (32-bit indexes)
+/// @brief   Constant pools map an integer to a constant.
+/// @details Can store approximately 4 billion constants (32-bit indexes).
 typedef struct hvm_constant_pool {
+  /// Entries in the pool: array of pointers to object references.
   struct hvm_object_ref** entries;
+  /// Index of the next index for an entry to be inserted at (ie. the length).
   uint32_t next_index;
+  /// Number of entries in the pool.
   uint32_t size;
 } hvm_constant_pool;
-// Start with 128 slots in the constant pool
+
+/// Start with 128 slots in the constant pool
+/// @relates hvm_constant_pool
 #define HVM_CONSTANT_POOL_INITIAL_SIZE 128
+/// @relates hvm_constant_pool
 #define HVM_CONSTANT_POOL_GROWTH_RATE  2
 
 #define HVM_GENERAL_REGISTERS 128
@@ -54,19 +56,26 @@ typedef struct hvm_constant_pool {
 typedef struct hvm_vm {
   // root stack
   // code
-  uint64_t ip; // instruction pointer (indexes bytes in the program)
   
-  byte* program; // data for instructions
-  uint64_t program_size; // size of program memory (in bytes)
+  /// Instruction pointer (indexes bytes in the program)
+  uint64_t ip;
+  /// Data for instructions
+  byte* program;
+  /// Size of program memory (in bytes)
+  uint64_t program_size;
   
   hvm_constant_pool const_pool;
-  
+  /// General purpose registers ($r0...$rN)
   struct hvm_obj_ref* general_regs[HVM_GENERAL_REGISTERS];
   // heap
   // object space
 } hvm_vm;
 
+/// Create a new virtual machine.
+/// @memberof hvm_vm
 hvm_vm *hvm_new_vm();
+/// Begin executing the virtual machine.
+/// @memberof hvm_vm
 void hvm_vm_run(hvm_vm*);
 
 /// Opcodes
