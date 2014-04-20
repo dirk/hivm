55297441b94bb139f6b0fc70a93e312e83c156ff
Dirk Gadsden <dirk@esherido.com>
2014-04-20 07:22:38 -0400

    Add integer-to-string primitive

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -6,6 +6,7 @@
 #include "object.h"
 #include "symbol.h"
 #include "bootstrap.h"
+#include "gc1.h"
 
 #define SYM(V) hvm_symbolicate(vm->symbols, V)
 #define PRIM_SET(K, V) hvm_obj_struct_internal_set(vm->primitives, SYM(K), (void*)V);
@@ -16,7 +17,8 @@ void hvm_bootstrap_primitives(hvm_vm *vm) {
   prim = hvm_prim_print;
   PRIM_SET("print", prim);
 
-  prim = hvm_prim_exit;
+  PRIM_SET("int_to_string", hvm_prim_int_to_string);
+
   PRIM_SET("exit", hvm_prim_exit);
 }
 
@@ -31,4 +33,17 @@ hvm_obj_ref *hvm_prim_print(hvm_vm *vm) {
   hvm_obj_string *str = strref->data.v;
   fputs(str->data, stdout);
   return hvm_const_null;
+}
+
+hvm_obj_ref *hvm_prim_int_to_string(hvm_vm *vm) {
+  hvm_obj_ref *intref = vm->param_regs[0];
+  assert(intref != NULL);
+  assert(intref->type == HVM_INTEGER);
+  int64_t intval = intref->data.i64;
+  char buff[24];// Enough to show a 64-bit signed integer in base 10
+  int err = sprintf(buff, "%lld", intval);
+  assert(err >= 0);
+  hvm_obj_ref *str = hvm_new_obj_ref_string_data(hvm_util_strclone(buff));
+  hvm_obj_space_add_obj_ref(vm->obj_space, str);
+  return str;
 }
--- src/bootstrap.h
+++ src/bootstrap.h
@@ -3,6 +3,7 @@
 
 void hvm_bootstrap_primitives(hvm_vm *vm);
 
+hvm_obj_ref *hvm_prim_int_to_string(hvm_vm *vm);
 hvm_obj_ref *hvm_prim_exit(hvm_vm *vm);
 hvm_obj_ref *hvm_prim_print(hvm_vm *vm);
 
