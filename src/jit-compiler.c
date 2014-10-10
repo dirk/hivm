ec3a4d88e14b2c13699132317355908f65431020
Dirk Gadsden <dirk@esherido.com>
2014-10-09 23:10:34 -0400

    Add compilation of set-symbol instruction

--- src/jit-compiler.c
+++ src/jit-compiler.c
@@ -27,15 +27,50 @@ LLVMModuleRef hvm_get_llvm_module() {
   return hvm_shared_llvm_module;
 }
 
-void hvm_jit_compile_resolve_registers(hvm_call_trace *trace, hvm_compile_bundle *bundle) {
+void hvm_jit_compile_resolve_registers(hvm_vm *vm, hvm_call_trace *trace, hvm_compile_bundle *bundle) {
+
+  // Sequence data items for the instruction that set a given register.
+  hvm_compile_sequence_data *general_reg_data_sources[HVM_GENERAL_REGISTERS];
+  // Actual values in a register
+  LLVMValueRef general_reg_values[HVM_GENERAL_REGISTERS];
+
+  byte reg;
   hvm_compile_sequence_data *data = bundle->data;
+
   for(unsigned int i = 0; i < trace->sequence_length; i++) {
-    hvm_compile_sequence_data *item = &data[i];
+    hvm_compile_sequence_data *data_item  = &data[i];
+    hvm_trace_sequence_item   *trace_item = &trace->sequence[i];
     // Do stuff with the item based upon its type.
+    switch(trace_item->head.type) {
+      case HVM_TRACE_SEQUENCE_ITEM_SETSYMBOL:
+        reg = trace_item->setsymbol.reg;
+        // Copy over our basic information from the trace item
+        data_item->setsymbol.reg      = reg;
+        data_item->setsymbol.constant = trace_item->setsymbol.constant;
+        // Also compile our symbol as a LLVM value
+        hvm_obj_ref *ref = hvm_const_pool_get_const(&vm->const_pool, data_item->setsymbol.constant);
+        // Set up our pointer type (in the 0 address space)
+        LLVMTypeRef pointer_type = LLVMPointerType(LLVMVoidType(), 0);
+        // Integer constant wants an `unsigned long long`.
+        LLVMValueRef value = LLVMConstInt(pointer_type, (unsigned long long)ref, true);
+        // Save our new value into the data item.
+        data_item->setsymbol.value = value;
+        // TODO: Call a VM function to check this
+        if(reg <= 127) {
+          // Save the data item pointer and the value into the register tables.
+          general_reg_data_sources[reg] = data_item;
+          general_reg_values[reg]       = value;
+        } else {
+          // TODO: Handle other register types if necessary
+        }
+        break;
+      default:
+        break;
+    }
   }
 }
 
-void hvm_jit_compile_trace(hvm_call_trace *trace) {
+void hvm_jit_compile_trace(hvm_vm *vm, hvm_call_trace *trace) {
   LLVMContextRef context = hvm_get_llvm_context();
   LLVMModuleRef  module = hvm_get_llvm_module();
   // Builder that we'll write the instructions from our trace into
@@ -53,7 +88,7 @@ void hvm_jit_compile_trace(hvm_call_trace *trace) {
 
   // Resolve register references in instructions into concrete IR value
   // references.
-  hvm_jit_compile_resolve_registers(trace, &bundle);
+  hvm_jit_compile_resolve_registers(vm, trace, &bundle);
 
   // Identify and extract gets/sets of globals and locals into dedicated
   // in-out pointers arguments to the block so that they can be passed
--- src/jit-compiler.h
+++ src/jit-compiler.h
@@ -11,6 +11,9 @@ typedef enum {
 
 #define HVM_COMPILE_DATA_HEAD hvm_compile_data_type type;
 
+// Data item conventions:
+//   .reg: Register that the result of the instruction goes in.
+
 typedef struct hvm_compile_sequence_data_arrayset {
   HVM_COMPILE_DATA_HEAD;
   // Source-values for the data we're going to use to perform the array set
@@ -20,10 +23,20 @@ typedef struct hvm_compile_sequence_data_arrayset {
   LLVMValueRef value;
 } hvm_compile_sequence_data_arrayset;
 
+typedef struct hvm_compile_sequence_data_setsymbol {
+  // Register that the `.symbol` would be placed into.
+  byte reg;
+  // The symbol that was retrieved up from the constant table.
+  LLVMValueRef value;
+  // Index into the constant table
+  uint32_t constant;
+} hvm_compile_sequence_data_setsymbol;
+
 /// Structs used for figuring out and keeping track of data related to each
 /// sequence in the trace instruction sequence being compiled.
 typedef union hvm_compile_sequence_data {
-  hvm_compile_sequence_data_arrayset arrayset;
+  hvm_compile_sequence_data_arrayset  arrayset;
+  hvm_compile_sequence_data_setsymbol setsymbol; 
 } hvm_compile_sequence_data;
 
 /// Holds all information relevant to a compilation of a trace (eg. instruction
@@ -34,10 +47,9 @@ typedef struct hvm_compile_bundle {
 } hvm_compile_bundle;
 
 // External API
-void hvm_jit_compile_trace(hvm_call_trace*);
+void hvm_jit_compile_trace(hvm_vm*, hvm_call_trace*);
 
 // Compiler internals
-void hvm_jit_compile_resolve_registers(hvm_call_trace*, hvm_compile_bundle*);
-void hvm_jit_compile_trace(hvm_call_trace*);
+void hvm_jit_compile_resolve_registers(hvm_vm*, hvm_call_trace*, hvm_compile_bundle*);
 
 #endif
